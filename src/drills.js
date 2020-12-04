bd22734f3080d69c559dc3583000ed0172e62595
kittybagpipes <sparticus978@gmail.com>
2020-12-04 09:27:11 -0800

    added searchByItemName

--- src/drills.js
+++ src/drills.js
@@ -1,7 +1,19 @@
-require("dontenv").config();
+require("dotenv").config();
 const knex = require("knex");
 
 const knexInstance = knex({
   client: "pg",
   connection: process.env.DB_URL,
 });
+
+function searchByItemName(searchTerm) {
+  knexInstance
+    .select("id", "name", "price", "date_added", "checked", "category")
+    .from("shopping_list")
+    .where("name", "ILIKE", `%${searchTerm}%`)
+    .then((result) => {
+      console.log(result);
+    });
+}
+
+searchByItemName("Tofurkey");
