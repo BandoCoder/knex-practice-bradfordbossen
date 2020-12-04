74f9ad611a89e541bbbaa0e6fdcfcbd8262efe56
kittybagpipes <sparticus978@gmail.com>
2020-12-03 22:10:23 -0800

    first knex instance

--- src/practice.js
+++ src/practice.js
@@ -1,1 +1,19 @@
-console.log('hello, practice')
+require("dotenv").config();
+const knex = require("knex");
+
+const knexInstance = knex({
+  client: "pg",
+  connection: process.env.DB_URL,
+});
+
+const qry = knexInstance
+  .select("product_id", "name", "price", "category")
+  .from("amazong_products")
+  .where({ name: "Point of view gun" })
+  .first()
+  .toQuery();
+//   .then((result) => {
+//     console.log(result);
+//   });
+
+console.log(qry);
