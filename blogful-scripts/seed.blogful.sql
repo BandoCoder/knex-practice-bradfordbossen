54c465757dd8458ea969128bc811377c23dcaad9
kittybagpipes <sparticus978@gmail.com>
2020-12-05 07:14:06 -0800

    practice

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,24 @@
+INSERT INTO blogful_articles
+    (title, date_published, content)
+VALUES
+('Story books', now() - '29days'::INTERVAL, 'Content Goes here I aint chainging it'),
+('Blurbs', now() - '28days'::INTERVAL, 'Content Goes here I aint chainging it'),
+('The Bando', now() - '27days'::INTERVAL, 'Content Goes here I aint chainging it'),
+('Rando Cardrissian', now() - '26days'::INTERVAL, 'Content Goes here I aint chainging it'),
+('Exit', now() - '26days'::INTERVAL, 'Content Goes here I aint chainging it'),
+('Bubbles', now() - '25days'::INTERVAL, 'Content Goes here I aint chainging it'),
+('What', now() - '24days'::INTERVAL, 'Content Goes here I aint chainging it'),
+('Screaming babies', now() - '23days'::INTERVAL, 'Content Goes here I aint chainging it'),
+('Foo', now() - '10days'::INTERVAL, 'Content Goes here I aint chainging it'),
+('Bar', now() - '9days'::INTERVAL, 'Content Goes here I aint chainging it'),
+('Pancake', now() - '9days'::INTERVAL, 'Content Goes here I aint chainging it'),
+('This is not ok', now() - '9days'::INTERVAL, 'Content Goes here I aint chainging it'),
+('Whatever', now() - '9days'::INTERVAL, 'Content Goes here I aint chainging it'),
+('Nevermine', now() - '8days'::INTERVAL, 'Content Goes here I aint chainging it'),
+('Hello', now() - '7days'::INTERVAL, 'Content Goes here I aint chainging it'),
+('Why is that', now() - '6days'::INTERVAL, 'Content Goes here I aint chainging it'),
+('Your child is the devil', now() - '5days'::INTERVAL, 'Content Goes here I aint chainging it'),
+('When does it end', now() - '4days'::INTERVAL, 'Content Goes here I aint chainging it'),
+('Food', now() - '3days'::INTERVAL, 'Content Goes here I aint chainging it'),
+('The Ride', now() - '1days'::INTERVAL, 'Content Goes here I aint chainging it'),
+('Generic', now() - '1days'::INTERVAL, 'Content Goes here I aint chainging it')
